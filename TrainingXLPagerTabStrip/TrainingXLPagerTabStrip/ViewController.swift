52c4f225462a9849bee6c6e025eca2bd6451baa6
iJoom <ghfdld@naver.com>
2020-04-18 01:00:08 +0900

    add 12차 스터디

--- TrainingXLPagerTabStrip/TrainingXLPagerTabStrip/ViewController.swift
+++ TrainingXLPagerTabStrip/TrainingXLPagerTabStrip/ViewController.swift
@@ -10,29 +10,32 @@ import UIKit
 import XLPagerTabStrip
 
 class ViewController: ButtonBarPagerTabStripViewController {
+    
 
     override func viewDidLoad() {
-        self.settings.style.buttonBarItemBackgroundColor = UIColor.white
-                self.settings.style.buttonBarItemTitleColor = UIColor.init(red: 213.0/255.0, green: 213.0/255.0, blue: 213.0/255.0, alpha: 1)
-                //선택된거 검은색 없음
-                self.settings.style.buttonBarBackgroundColor = UIColor.white
-        //            UIColor.init(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1)
-                
-                self.settings.style.selectedBarBackgroundColor = UIColor.init(red: 19.0/255.0, green: 139.0/255.0, blue: 255.0/255.0, alpha: 1)
-                self.settings.style.selectedBarHeight = 2.0
-                
-        self.settings.style.buttonBarHeight = 130
-                
-                changeCurrentIndexProgressive = { (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
-                    guard changeCurrentIndex == true else { return }
-                    
-                    oldCell?.label.textColor = UIColor.init(red: 213.0/255.0, green: 213.0/255.0, blue: 213.0/255.0, alpha: 1)
-                    newCell?.label.textColor = UIColor.black
-                    
-                   
-                }
-        
         super.viewDidLoad()
+//        self.settings.style.buttonBarItemBackgroundColor = UIColor.white
+//        self.settings.style.buttonBarItemTitleColor = UIColor.init(red: 213.0/255.0, green: 213.0/255.0, blue: 213.0/255.0, alpha: 1)
+//        //선택된거 검은색 없음
+//        self.settings.style.buttonBarBackgroundColor = UIColor.white
+//        //            UIColor.init(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1)
+//        self.settings.style.selectedBarBackgroundColor = UIColor.init(red: 19.0/255.0, green: 139.0/255.0, blue: 255.0/255.0, alpha: 1)
+//        self.settings.style.selectedBarHeight = 2.0
+//
+//        self.settings.style.buttonBarHeight = 60
+//
+//        changeCurrentIndexProgressive = { (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
+//            guard changeCurrentIndex == true else { return }
+//
+//            oldCell?.label.textColor = UIColor.init(red: 213.0/255.0, green: 213.0/255.0, blue: 213.0/255.0, alpha: 1)
+//            newCell?.label.textColor = UIColor.black
+//
+            
+//        }
+        
+        
+        
+        
         // Do any additional setup after loading the view.
     }
     
