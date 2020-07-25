3d91098c0d2fe6fc04965858dad72f3373e0cac8
iJoom <ghfdld@naver.com>
2020-07-25 15:58:46 +0900

    create CodeViewProject

---
+++ PracticeCodeView/Pods/Target Support Files/Pods-PracticeCodeView/Pods-PracticeCodeView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PracticeCodeView : NSObject
+@end
+@implementation PodsDummy_Pods_PracticeCodeView
+@end
---
+++ PracticeCodeView/PracticeCodeView/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  PracticeCodeView
+//
+//  Created by IJ . on 2020/07/25.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import Then
+class ViewController: UIViewController {
+
+    
+    let nameLabel = UILabel().then {
+        $0.text = "nameLabel"
+        $0.textColor = .blue
+        $0.textAlignment = .center
+        $0.font = UIFont.systemFont(ofSize: 18)
+    }
+    let nameTextField = UITextField()
+    let changeButton = UIButton()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let redView = UIView()
+        redView.backgroundColor = .systemRed
+        self.view.addSubview(redView)
+        self.view.addSubview(self.nameLabel)
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            redView.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor),
+//            redView.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor),
+//            redView.widthAnchor.constraint(equalToConstant: 100),
+//            redView.heightAnchor.constraint(equalToConstant: 600)
+//        ])
+        redView.snp.makeConstraints {
+           // $0.centerX.centerY.equalTo(self.view.safeAreaLayoutGuide)
+            $0.width.height.equalTo(150)
+        }
+        self.nameLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        
+    }
+
+
+}
+
